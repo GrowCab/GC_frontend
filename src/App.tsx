bb20d71ca977456d8e8009736c5ad121579e1ab9
ljyanesm <yanes.luis@gmail.com>
2021-04-13 21:04:25 +0100

    Separating unit configurations
    First attempt at separating each 'unit' configuration into different components by filtering the ExpectedMeasures using the units available for the chamber (This is hardcoded to 1 for now)
    React seemed to be missing from package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './App.css'
-import { GetChamberSchedule, GetSensors } from './Api_spec/generated-types'
+import { GetChamberSchedule, GetSensors, useGetChamberUnits } from './Api_spec/generated-types'
 import ExpectedMeassureInterval from './ExpectedMeassureInterval'
 
 const App = () => {
-
+  const chamberUnits = useGetChamberUnits({chamber_id: 1}).data
   return <div className='App'>
     <header className='App-header'>
       <p>Sensors list:</p>
@@ -22,12 +22,24 @@ const App = () => {
     <div> Chamber 1 schedule:
       <GetChamberSchedule chamber_id={1} children={(configuration, action) => {
         return (action.loading) ? <p>loading...</p> :
-          <div key={configuration?.id}> Intervals:
-            {configuration?.expected_measure?.map((expected_measure, idx, expected_measures) => (
-              //<ExpectedMeassureInterval expectedMeasure={expected_measure} idx={idx}  expectedMeasures={expected_measures} />
-              <ExpectedMeassureInterval  expectedMeasure={expected_measure} idx={idx}  expectedMeasures={expected_measures} />
-              )
-            )}
+          <div key={configuration?.id}>
+            {
+              chamberUnits?.map((unit) => {
+                return (
+                  <div>
+                    <p>Unit {unit.description}</p>
+                    {configuration?.expected_measure?.filter(
+                      (expected_measure, idx, expected_measures) => (expected_measure.unit_id === unit.id))
+                      .map(
+                        (expected_measure, idx, expected_measures) =>
+                          (
+                            <ExpectedMeassureInterval expectedMeasure={expected_measure} idx={idx} expectedMeasures={expected_measures}/>
+                          )
+                      )
+                    }
+                  </div>
+                )})
+            }
           </div>
       }}/>
     </div>
